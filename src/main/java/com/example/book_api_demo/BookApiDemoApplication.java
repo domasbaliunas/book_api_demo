59761c134fccb5412cc099fdfd791c39cb1bb37e
Domas Baliūnas <domas.baliunas@gmail.com>
2024-09-28 14:35:39 +0300

    Initial commit

---
+++ src/main/java/com/example/book_api_demo/BookApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.book_api_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookApiDemoApplication.class, args);
+	}
+
+}
